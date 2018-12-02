931f560d0d5873c7d762f28029b78eecbfaa41b4
Camilla Camatta <camillacamatta@gmail.com>
2018-12-02 21:09:50 -0200

    Music Added

---
+++ ColheitaMaldita/Assets/Scripts/Behaviours/RandomMusicBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMusicBehaviour : MonoBehaviour {
+
+
+    public AudioClip en;
+    public AudioClip pt;
+    public AudioSource audiosrc;
+
+    void Start () {
+        PlayRandom();
+    }
+
+    void PlayRandom()
+    {
+        Invoke("PlayMusic", Random.Range(60, 180));
+    }
+
+    void PlayMusic()
+    {
+        if(XMLLanguageLoader.selectedLanguage == "English")
+        {
+            audiosrc.PlayOneShot(en);
+        }
+        else
+        {
+
+            audiosrc.PlayOneShot(pt);
+        }
+
+        PlayRandom();
+    }
+}
