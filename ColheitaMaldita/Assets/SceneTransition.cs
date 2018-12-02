60ba4ae46d7a3e4186c7c807655cfe0e10af9860
Camilla Camatta <camillacamatta@gmail.com>
2018-12-02 16:27:09 -0200

    Screen transitions added

---
+++ ColheitaMaldita/Assets/SceneTransition.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneTransition : MonoBehaviour {
+
+    bool starting = true;
+    Image filledTransition;
+    // Use this for initialization
+    void Start () {
+        filledTransition = GetComponent<Image>();
+	}
+	
+	void Update () {
+        if (starting)
+        {
+            filledTransition.fillAmount -= Time.deltaTime * 2;
+            filledTransition.fillClockwise = true;
+            if (filledTransition.fillAmount <= 0)
+            {
+                starting = false;
+            }
+        }
+    }
+}
